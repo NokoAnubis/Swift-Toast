dbe00e3527c86b42b312531f6c5ce5f4607bc0cd
Joel Joseph <joeljordanjoseph@gmail.com>
2023-07-28 21:28:18 -0600

    more name changes

--- Package.swift
+++ Package.swift
@@ -10,15 +10,12 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftToast",
-            targets: ["swift-toast"]),
+            targets: ["SwiftToast"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "swift-toast"),
-        .testTarget(
-            name: "swift-toastTests",
-            dependencies: ["swift-toast"]),
+            name: "SwiftToast"),
     ]
 )
