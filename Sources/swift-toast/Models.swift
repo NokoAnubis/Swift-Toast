9008dd42c935ba2c3778a19a49fd70aa5fb150c0
Joel Joseph <joeljordanjoseph@gmail.com>
2023-07-28 21:14:27 -0600

    version 0.0.1 ready

--- Sources/swift-toast/Models.swift
+++ Sources/swift-toast/Models.swift
@@ -1,8 +1,28 @@
 //
-//  File.swift
-//  
+//  Models.swift
+//  Olympsis
 //
-//  Created by Joel Joseph on 7/17/23.
+//  Created by Joel Joseph on 7/15/23.
 //
 
 import Foundation
+
+struct Toast: Equatable {
+    var style: ToastStyle
+    var actor: String?
+    var title: String
+    var message: String
+    var duration: Double = 3
+    var width: Double = SCREEN_WIDTH
+}
+
+enum ToastStyle: String {
+    case error = "error"
+    case warning = "warning"
+    case success = "success"
+    case info = "info"
+    case newPost = "new_post"
+    case message = "message"
+    case newEvent = "new_event"
+    case eventStatus = "event_status"
+}
--- Sources/swift-toast/ToastViewModifier.swift
+++ Sources/swift-toast/ToastViewModifier.swift
@@ -1,18 +1,217 @@
 //
-//  SwiftUIView.swift
-//  
+//  ToastViewModifier.swift
+//  Olympsis
 //
-//  Created by Joel Joseph on 7/17/23.
+//  Created by Joel Joseph on 7/16/23.
 //
 
 import SwiftUI
+import Foundation
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct ToastViewModifier: ViewModifier {
+    
+    @Binding var isPresented: Bool
+    @Binding var toast: Toast
+    
+    enum DisplayPosition {
+        case top
+        case bottom
     }
+    
+    func body(content: Content) -> some View {
+        switch toast.style {
+        case .error:
+            return content
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .overlay(alignment: .bottom) {
+                    ToastViewer()
+                        .onChange(of: isPresented) { newValue in
+                            guard newValue == true else {
+                                return
+                            }
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                                withAnimation(.easeInOut) {
+                                    isPresented = false
+                                }
+                            }
+                        }
+                }
+        case .warning:
+            return content
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .overlay(alignment: .bottom) {
+                    ToastViewer()
+                        .onChange(of: isPresented) { newValue in
+                            guard newValue == true else {
+                                return
+                            }
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                                withAnimation(.easeInOut) {
+                                    isPresented = false
+                                }
+                            }
+                        }
+                }
+        case .success:
+            return content
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .overlay(alignment: .bottom) {
+                    ToastViewer()
+                        .onChange(of: isPresented) { newValue in
+                            guard newValue == true else {
+                                return
+                            }
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                                withAnimation(.easeInOut) {
+                                    isPresented = false
+                                }
+                            }
+                        }
+                }
+        case .info:
+            return content
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .overlay(alignment: .bottom) {
+                    ToastViewer()
+                        .onChange(of: isPresented) { newValue in
+                            guard newValue == true else {
+                                return
+                            }
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                                withAnimation(.easeInOut) {
+                                    isPresented = false
+                                }
+                            }
+                        }
+                }
+        case .newPost:
+            return content
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .overlay(alignment: .top) {
+                    ToastViewer()
+                        .onChange(of: isPresented) { newValue in
+                            guard newValue == true else {
+                                return
+                            }
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                                withAnimation(.easeInOut) {
+                                    isPresented = false
+                                }
+                            }
+                        }
+                }
+        case .message:
+            return content
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .overlay(alignment: .top) {
+                    ToastViewer()
+                        .onChange(of: isPresented) { newValue in
+                            guard newValue == true else {
+                                return
+                            }
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                                withAnimation(.easeInOut) {
+                                    isPresented = false
+                                }
+                            }
+                        }
+                }
+        case .newEvent:
+            return content
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .overlay(alignment: .top) {
+                    ToastViewer()
+                        .onChange(of: isPresented) { newValue in
+                            guard newValue == true else {
+                                return
+                            }
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                                withAnimation(.easeInOut) {
+                                    isPresented = false
+                                }
+                            }
+                        }
+                }
+        case .eventStatus:
+            return content
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .overlay(alignment: .top) {
+                    ToastViewer()
+                        .onChange(of: isPresented) { newValue in
+                            guard newValue == true else {
+                                return
+                            }
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                                withAnimation(.easeInOut) {
+                                    isPresented = false
+                                }
+                            }
+                        }
+                }
+        }
+        
+    }
+    
+    @ViewBuilder func ToastViewer() -> some View {
+        if isPresented {
+            switch toast.style {
+            case .error:
+                withAnimation {
+                    ToastView(toast: toast)
+                        .offset(y: -20)
+                        .transition(.offset(y: 1000))
+                }
+            case .warning:
+                withAnimation {
+                    ToastView(toast: toast)
+                        .offset(y: -20)
+                        .transition(.offset(y: 1000))
+                }
+            case .success:
+                withAnimation {
+                    ToastView(toast: toast)
+                        .offset(y: -20)
+                        .transition(.offset(y: 1000))
+                }
+            case .info:
+                withAnimation {
+                    ToastView(toast: toast)
+                        .offset(y: -20)
+                        .transition(.offset(y: 1000))
+                }
+            case .newPost:
+                withAnimation {
+                    ToastView(toast: toast)
+                        .offset(y: 15)
+                        .transition(.offset(y: -1000))
+                }
+            case .message:
+                withAnimation {
+                    ToastView(toast: toast)
+                        .offset(y: 15)
+                        .transition(.offset(y: -1000))
+                }
+            case .newEvent:
+                withAnimation {
+                    ToastView(toast: toast)
+                        .offset(y: 15)
+                        .transition(.offset(y: -1000))
+                }
+            case .eventStatus:
+                withAnimation {
+                    ToastView(toast: toast)
+                        .offset(y: 15)
+                        .transition(.offset(y: -1000))
+                }
+            }
+        }
+    }
+      
 }
 
-#Preview {
-    SwiftUIView()
+extension View {
+
+    func toast(isPresented: Binding<Bool>, toast: Binding<Toast>) -> some View {
+        self.modifier(ToastViewModifier(isPresented: isPresented, toast: toast))
+    }
 }
