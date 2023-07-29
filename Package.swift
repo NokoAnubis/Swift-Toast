920e4fe83f202e6c93348fe7e62836bd24656af4
Joel Joseph <joeljordanjoseph@gmail.com>
2023-07-28 21:24:56 -0600

    testing name change

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-toast",
+    name: "SwiftToast",
     platforms: [.iOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
